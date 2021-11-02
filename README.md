cf81e1ea9feab71aa1ab7c3f67cce11e7461baf4
imamhossain94 <imamagun94@gmail.com>
2021-11-02 18:06:20 +0600

    feat: README.md file updated

--- README.md
+++ README.md
@@ -14,3 +14,30 @@ pip install requests
 
 ### CSV
   <td width="100%"><img src="https://github.com/imamhossain94/pdf-to-data-table-extractor/blob/main/csv_data.png" alt="BLANK"></td>
+
+### First Table ~ CSV-To-JSON
+```json
+{
+   "Year":[
+      "2017", "2018", "2019", "2020"
+   ],
+   "Susan":[
+      "570", "647", "343", "425"
+   ],
+   "Gerald":[
+      "635", "325", "680", "542"
+   ],
+   "Bobbie":[
+      "684", "319", "687", "553"
+   ],
+   "Keisha":[
+      "397", "601", "447", "477"
+   ],
+   "Art":[
+      "678", "520", "674", "648"
+   ]
+}
+```
+
+Note: You have to write code to generate formatted json.
+<a href="https://docs.python.org/3/library/csv.html">Click to know more</a>
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from requests import get
 import tabula
+import csv
+import json
 
 
 def extract_table_from_pdf():
@@ -13,11 +15,34 @@ def extract_table_from_pdf():
             # Read & print data
             csv_data = tabula.read_pdf("table.pdf", format='csv', pages='all', lattice=True, silent=True)
             print(len(csv_data))
-            print(csv_data[0])
+            # print(csv_data)
 
             # Convert & save data in csv
             tabula.convert_into("table.pdf", "data.csv", output_format="csv", pages='all')
 
+            # Convert csv to json
+            data = {}
+            with open("data.csv") as csv_file:
+                # csv_reader = csv.DictReader(csv_file)
+                # for rows in csv_reader:
+                #     # xd = rows['0']
+                #     print(rows['Susan'])
+                # reader = csv.reader(csv_file)
+                # for rows in reader:
+                #     print(reader.line_num)
+
+                # Extract first table data
+                data = {}
+                i = 0
+                for row in csv.DictReader(csv_file):
+                    for key, value in row.items():
+                        if i == 24:
+                            break
+                        if key not in data:
+                            data[key] = []
+                        data[key].append(value)
+                        i += 1
+                print(data)
             break
         except Exception as e:
             print(e)
@@ -25,3 +50,6 @@ def extract_table_from_pdf():
 
 
 extract_table_from_pdf()
+
+# Note: Maybe there have more efficient way to read csv file
+# Please contribute it or suggest me.
